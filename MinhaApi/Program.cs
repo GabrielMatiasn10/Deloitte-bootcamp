ea003da10988689af39c96c61e26fbce3dad79ad
Rene Rojas <re.renerojas@gmail.com>
2026-02-05 18:07:27 -0300

    Alterações e inclusoes

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -1,12 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MinhaApi.Data; 
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+// Registro do DbContext com Npgsql
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var cs = builder.Configuration.GetConnectionString("DefaultConnection"); // ou "Postgres", conforme você padronizou
+    options
+        .UseNpgsql(cs)
+        .UseSnakeCaseNamingConvention(); // <- esta linha resolve a conversão para snake_case
+});
+
+
+// Recomendação do Npgsql para compatibilidade de timestamp (se aplicável)
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
-
-app.MapControllers();
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -15,29 +31,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Mapear controllers
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
